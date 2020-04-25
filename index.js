51a822f9ec6566624e5db376188a5d521e56965f
Rasha Saadeh <>
2020-04-25 07:43:06 -0400

    Hopefully working App

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const app = express();
 require('dotenv').config();
 
 
-// var cors = require('cors');
-// app.use(cors());
+var cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
